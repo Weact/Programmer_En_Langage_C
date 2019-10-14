2a330161bdff69dcc1ed2aff16da91f37d453cbc
Weaact <lucas.druckes@numericable.fr>
2019-10-14 17:01:55 +0200

    Ludus Academie - Programmer en Langage C - 14 10 2019 - 17h01 Version : Early Access 1.0

--- Programmer_En_C_1/main.c
+++ Programmer_En_C_1/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+#define NFOIS 5
+
+int main(int argc, char *argv[]) // 1
 {
-    printf("Hello world!\n");
+    int i;
+    float x;
+    float racx;
+
+    printf("Bonjour\n");
+
+    printf("Je vais vous calculer %d racines carree\n",NFOIS);
+
+    for(i = 0; i < NFOIS; i++){
+        printf("Donnez un nombre : ");
+        scanf("%f", &x);
+        if(x < 0.0)
+            printf("Le nombre %f ne possède pas de racine carree\n", x);
+        else{
+            racx=sqrt(x);
+            printf("Le nombre %f a pour racine carree : %f\n",x,racx);
+        }
+
+    }
+    printf("Travail Termine - Au revoir");
+
     return 0;
 }
